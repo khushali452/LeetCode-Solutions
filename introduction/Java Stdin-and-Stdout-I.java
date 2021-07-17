a827d10eab6f5c6b1fa4deb3ae4b91792ef231de
khushali452 <khushalivasani.ict19@gmail.com>
2021-07-17 12:01:15 +0530

    introduction

---
+++ introduction/Java Stdin-and-Stdout-I.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class HackerRank_StdInStdOut1 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        int z = sc.nextInt();
+        sc.close();
+        
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(z);
+        
+    }
+}
+© 2021 GitHub, Inc.
