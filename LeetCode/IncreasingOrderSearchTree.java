23688f3ad0f3afa4e0f55b62ccba9564af683bbe
khushali452 <khushalivasani.ict19@gmail.com>
2021-11-27 17:14:25 +0530

    .

---
+++ LeetCode/IncreasingOrderSearchTree.java
@@ -0,0 +1,54 @@
+/* 
+897. Increasing Order Search Tree
+
+Given the root of a binary search tree, rearrange the tree in in-order so that the leftmost node in the tree is now the root of the tree, and every node has no left child and only one right child.
+
+ Example 1:
+
+Input: root = [5,3,6,2,4,null,8,1,null,null,null,7,9]
+Output: [1,null,2,null,3,null,4,null,5,null,6,null,7,null,8,null,9]
+Example 2:
+
+
+Input: root = [5,1,7]
+Output: [1,null,5,null,7]
+ 
+
+Constraints:
+
+The number of nodes in the given tree will be in the range [1, 100].
+0 <= Node.val <= 1000
+*/
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode increasingBST(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        inorder(root,list);
+        TreeNode ans=new TreeNode(0),cur=ans;
+        for(int i:list){
+            cur.right=new TreeNode(i);
+            cur=cur.right;
+        }
+        return ans.right;
+    }
+     public void inorder(TreeNode node, List<Integer> list) {
+        if (node == null) return;
+        inorder(node.left, list);
+        list.add(node.val);
+        inorder(node.right, list);
+    }
+}
