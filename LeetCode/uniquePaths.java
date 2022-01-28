35979d4187e919750c054f24d0e3afee43cf2c07
khushali452 <khushalivasani.ict19@gmail.com>
2022-01-28 12:33:44 +0530

    .

---
+++ LeetCode/uniquePaths.java
@@ -0,0 +1,45 @@
+/* 
+62. Unique Paths
+
+There is a robot on an m x n grid. The robot is initially located at the top-left corner (i.e., grid[0][0]). The robot tries to move to the bottom-right corner (i.e., grid[m - 1][n - 1]). The robot can only move either down or right at any point in time.
+
+Given the two integers m and n, return the number of possible unique paths that the robot can take to reach the bottom-right corner.
+
+The test cases are generated so that the answer will be less than or equal to 2 * 109.
+
+ 
+
+Example 1:
+
+
+Input: m = 3, n = 7
+Output: 28
+Example 2:
+
+Input: m = 3, n = 2
+Output: 3
+Explanation: From the top-left corner, there are a total of 3 ways to reach the bottom-right corner:
+1. Right -> Down -> Down
+2. Down -> Down -> Right
+3. Down -> Right -> Down
+ 
+
+Constraints:
+
+1 <= m, n <= 100
+*/
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int[][] path_matrix = new int[m][n];
+        
+        for(int i = m - 1; i >= 0; i--){
+            for(int j = n - 1; j >= 0; j--){
+                if(i == m - 1 || j == n - 1)
+                    path_matrix[i][j] = 1;
+                else
+                    path_matrix[i][j] = path_matrix[i][j+1] + path_matrix[i+1][j];
+            }
+        }
+        return path_matrix[0][0];
+    }
+}
