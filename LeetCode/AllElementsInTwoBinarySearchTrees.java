fe4f5e4529e80fd2b30ac52e60138afb65f99f3f
khushali452 <khushalivasani.ict19@gmail.com>
2022-02-20 13:44:26 +0530

    .

---
+++ LeetCode/AllElementsInTwoBinarySearchTrees.java
@@ -0,0 +1,69 @@
+/* 
+1305. All Elements in Two Binary Search Trees
+
+Given two binary search trees root1 and root2, return a list containing all the integers from both trees sorted in ascending order.
+
+ 
+
+Example 1:
+
+
+Input: root1 = [2,1,4], root2 = [1,0,3]
+Output: [0,1,1,2,3,4]
+Example 2:
+
+
+Input: root1 = [1,null,8], root2 = [8,1]
+Output: [1,1,8,8]
+ 
+
+Constraints:
+
+The number of nodes in each tree is in the range [0, 5000].
+-105 <= Node.val <= 105
+*/
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> getAllElements(TreeNode root1, TreeNode root2) {
+        Stack<TreeNode> st1 = new Stack<>();
+        Stack<TreeNode> st2 = new Stack<>();
+        
+        List<Integer> res = new ArrayList<>();
+        
+        while(root1 != null || root2 != null || !st1.empty() || !st2.empty()){
+            while(root1 != null){
+                st1.push(root1);
+                root1 = root1.left;
+            }
+            while(root2 != null){
+                st2.push(root2);
+                root2 = root2.left;
+            }
+            if(st2.empty() || (!st1.empty() && st1.peek().val <= st2.peek().val)){
+                root1 = st1.pop();
+                res.add(root1.val);
+                root1 = root1.right;
+            }
+            else{
+                root2 = st2.pop();
+                res.add(root2.val);
+                root2 = root2.right;
+            }
+        }
+        return res;
+    }
+}
