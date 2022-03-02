9e5d609af14a540f8267abff34ef5402363c3c2d
khushali452 <khushalivasani.ict19@gmail.com>
2022-03-03 00:47:40 +0530

    leetcode CP

--- HackerRank/introduction/Welcome-to-java.java
+++
@@ -1,8 +0,0 @@
-public class HackerRank_Welcometojava {
-
-    public static void main(String[] args) {
-        
-        System.out.println("Hello, World.");
-        System.out.println("Hello, Java.");
-    }
-}
