f345f987fdc1b7b82cdf767ff9fc657a053147bb
Khushali Vasani <62420933+khushali452@users.noreply.github.com>
2022-04-23 23:55:24 +0530

    Time: 2 ms (89.36%), Space: 42.1 MB (95.06%) - LeetHub

---
+++ 535-encode-and-decode-tinyurl/535-encode-and-decode-tinyurl.java
@@ -0,0 +1,14 @@
+public class Codec {
+    List<String> urls = new ArrayList<String>();
+    // Encodes a URL to a shortened URL.
+    public String encode(String longUrl) {
+        urls.add(longUrl);
+        return String.valueOf(urls.size()-1);
+    }
+
+    // Decodes a shortened URL to its original URL.
+    public String decode(String shortUrl) {
+        int index = Integer.valueOf(shortUrl);
+        return (index<urls.size())?urls.get(index):"";
+    }
+}
